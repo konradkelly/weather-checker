98114444e2e3cb77dbd1b766bd0c51eca975c6c4
konradkelly <konradky@gmail.com>
2025-04-10 15:51:06 -0700

    METHODS FOR MAX AND MIN TEMPS

--- src/WeatherSummary.java
+++ src/WeatherSummary.java
@@ -22,12 +22,27 @@ public class WeatherSummary {
         // Hint: use Scanner. nextDouble() and hasNextDouble() will be helpful here!
         Scanner scan = new Scanner(System.in);
 
+        double max = Double.NEGATIVE_INFINITY;
+        double min = Double.POSITIVE_INFINITY;
+
         while (scan.hasNextDouble()) {
 
             double temps = scan.nextDouble();
 
-            System.out.println(temps);
+            //System.out.println(temps);
+
+            if (temps > max) {
+                max = temps;
+            }
+
+            if (temps < min) {
+                min = temps;
+            }
         }
         scan.close();
+
+        System.out.println("The Maximum temperature is " + max);
+        System.out.println("The Minimum temperature is " + min);
+        
     }
 }
